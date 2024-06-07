c50129612843036fbf77534e7cf95494db114764
dearis02 <kdarista02@gmail.com>
2024-06-08 02:48:26 +0800

    feat: finished ui implementation

---
+++ app/src/main/java/bangkit/capstone/waterwise/news/types.kt
@@ -0,0 +1,22 @@
+package bangkit.capstone.waterwise.news
+import com.google.gson.annotations.SerializedName
+
+data class Types(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String
+)
+
+data class News(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("content")
+    val content: String,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("type")
+    val type: Types
+)
