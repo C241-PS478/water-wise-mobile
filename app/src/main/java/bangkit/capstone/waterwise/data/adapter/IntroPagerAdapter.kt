6f6194b10166476397612bd49c6813bbe00a0381
Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
2024-06-08 00:57:07 +0800

    pull main

---
+++ app/src/main/java/bangkit/capstone/waterwise/data/adapter/IntroPagerAdapter.kt
@@ -0,0 +1,32 @@
+package bangkit.capstone.waterwise.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import bangkit.capstone.waterwise.R
+import bangkit.capstone.waterwise.data.model.IntroItem
+import kotlinx.android.synthetic.main.item_intro_page.view.*
+
+class IntroPagerAdapter(private val introItems: List<IntroItem>) :
+    RecyclerView.Adapter<IntroPagerAdapter.IntroViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_intro, parent, false)
+        return IntroViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: IntroViewHolder, position: Int) {
+        holder.bind(introItems[position])
+    }
+
+    override fun getItemCount(): Int = introItems.size
+
+    class IntroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(introItem: IntroItem) {
+            itemView.imageView.setImageResource(introItem.image)
+            itemView.textViewTitle.text = introItem.title
+            itemView.textViewDescription.text = introItem.description
+        }
+    }
+}
